4ca36e8e88de7d272df035aa4ad2f12458596c96
Lakshamana <guitrompa1@gmail.com>
2020-01-19 18:51:23 -0300

    configure webpack+babel loaders

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './css/index.css'
 
 const App = () => {
   return <div>Hello World!</div>
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,10 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const htmlPlugin = new HtmlWebpackPlugin({
+  template: './public/index.html',
+  filename: './index.html'
+})
+
 module.exports = {
   module: {
     rules: [
@@ -5,7 +12,27 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: {
+                localIdentName: '[name]_[local]_[hash:base64]'
+              },
+              sourceMap: true
+            }
+          }
+        ]
       }
-    ]
-  }
+    ],
+  },
+  plugins: [htmlPlugin]
 }
